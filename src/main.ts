50a2f7e1949025e7af2659bdfe1dffbc6810351a
Ahmed <ahmed.hussein@zarcony.dev>
2024-07-08 15:14:57 +0300

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(MessagesModule);
+
+  // Pipe to validate the data sending to the sever and before the controllers.
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/messages/dtos/create-message.dto.ts
@@ -0,0 +1,7 @@
+import { IsString } from 'class-validator';
+
+// DTO => Data Transfer Object
+export class CreateMessageDto {
+  @IsString()
+  content: string;
+}
